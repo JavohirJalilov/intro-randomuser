8bb255a1771983b745b2648d507044921cab61e0
Jalilov <javohirjaliloff@gmail.com>
2023-02-02 17:38:04 +0500

    get users data

--- user.py
+++ user.py
@@ -16,7 +16,15 @@ def get_user(user_data: dict) -> dict:
             'country': user country
         }
     '''
-    pass
+    results = user_data['results'][0]
+    name = results['name']
+    data = {
+        'firstname': name['first'],
+        'lastname': name['last'],
+        'age': results['dob']['age'],
+        'country': results['location']['country']
+    }
+    return data
 
 
 def get_users(url: str, n: int) -> list:
@@ -29,15 +37,27 @@ def get_users(url: str, n: int) -> list:
     Returns:
         list: list of users. user from get_user()
     '''
-    pass
-    
+    users_data = []
+    for i in range(n):
+        r = requests.get(url)
+        data = r.json()
+        user = get_user(data)
+        users_data.append(user)
 
-def write_users_to_file(file_path: str, n: int):
+    return users_data
+
+def write_users_to_file(file_path: str, data:list):
     '''write n users to file
 
     Args:
         url (str): api url
-        n (int): number of users
+        data (list): number of users
     '''
-    pass
+    with open(file_path, 'w') as f:
+        data = json.dumps(data)
+        f.write(data)
+    
+url = 'https://randomuser.me/api/'
+data = get_users(url, 10)
 
+write_users_to_file('result.json', data)
